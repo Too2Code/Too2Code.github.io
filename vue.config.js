9b6f3871ff86f479a31b09b94ac74b8b0d04b8b5
Too2Code <100068712+Too2Code@users.noreply.github.com>
2022-02-22 10:38:38 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,7 @@ function resolve(dir) {
   return path.join(__dirname, dir);
 }
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/contract-deployer/'
-    : '/',
+  publicPath: '/',
   assetsDir: "static",
   runtimeCompiler: true,
   chainWebpack: config => {
